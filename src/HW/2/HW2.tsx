b2482798e45fb965af962bc2c4b082834d49c02a
Olga <oyakubovska80@gmail.com>
2025-04-01 11:34:56 +0200

    commit-002

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -15,7 +15,7 @@ export type UserType = {
 
 export type UsersObjectType = {
   myFriends: UserType[];
-   // пропиши типизацию
+  // пропиши типизацию
 };
 
 // export type UserList2PropsType = {
@@ -101,15 +101,8 @@ export const HW2 = () => {
 
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
-  // const filterUsers = () => {
-  //   // const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
-  //   const filteredUsers = users.myFriends.filter((user)=>user.address.city === 'Los Angeles');
-
-  //   setCurrentUsers({ myFriends: filteredUsers });
-  // };
   const filterUsers = () => {
-    // const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
-    const filteredUsers = users.myFriends.filter(
+    const filteredUsers = currentUsers.myFriends.filter(
       (user) => user.address.city === "Los Angeles"
     );
 
