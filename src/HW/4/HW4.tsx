77bac6a0e62778015efbec3282fb5f6f170c1be8
Olga <oyakubovska80@gmail.com>
2025-04-17 11:30:53 +0200

    commit-004

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import s from './HW4.module.css';
-import { Button } from './Button';
-import { Input } from './Input';
+import { useState } from "react";
+import S from "./HW4.module.css";
+import { Button } from "./Button";
+import { Input } from "./Input";
 
 export const HW4 = () => {
   // 1️⃣ Раскомментируйте JSX (HW4.tsx) и вы увидите,
@@ -14,19 +14,45 @@ export const HW4 = () => {
 
   // ❗ Массив с данными не трогаем! Текст не меняем !
 
-  const [currentText, setCurrentText] = useState('');
+  const [currentText, setCurrentText] = useState("");
   const [texts, setTexts] = useState<string[]>([
-    'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
+    "То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)",
   ]);
 
   const handleSave = () => {
     setTexts([currentText, ...texts]);
-    setCurrentText('');
+    setCurrentText("");
   };
 
   return (
-    <div id={'hw04'}>
-      {/*{currentText ? (*/}
+    <div id={"hw04"}>
+      {currentText ? (
+        <h1 id={"hw04-text"}>{currentText}</h1>
+      ) : (
+        <h1 id={"hw04-default-text"}>Здесь появится новое дело</h1>
+      )}
+
+      <Input currentText={currentText} setCurrentText={setCurrentText} />
+
+      <Button callBack={handleSave} name={"click"} />
+
+      <h1 style={{ marginTop: "50px" }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
+
+      <ol id={"hw04-tasks"}>
+        {texts.map((el, index) => {
+          return (
+            <li
+              key={index}
+              id={`hw04-task-${index}`}
+              className={index % 2 === 0 ? S.chetNechet : ""}
+            >
+              {el}
+            </li>
+          );
+        })}
+      </ol>
+
+      {/* {currentText ? (*/}
       {/*  <h1 id={'hw04-text'}>{currentText}</h1>*/}
       {/*) : (*/}
       {/*  <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>*/}
@@ -46,8 +72,7 @@ export const HW4 = () => {
       {/*      </li>*/}
       {/*    );*/}
       {/*  })}*/}
-      {/*</ol>*/}
+      {/*</ol> */}
     </div>
   );
 };
-
